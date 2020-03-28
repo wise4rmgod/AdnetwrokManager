a3cd2973174b60ef7691eac190b9b25dede05b0b
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2020-03-28 10:13:01 +0100

    Initial commit

---
+++ adnetworkm/src/main/java/com/dev/adnetworkm/CheckNetworkStatus.kt
@@ -0,0 +1,39 @@
+package com.dev.adnetworkm
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import android.os.Build
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+object CheckNetworkStatus : ViewModel() {
+    val networklivedata: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun getNetworkLiveData(context: Context): LiveData<Boolean> {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network?) {
+                networklivedata.postValue(true)
+
+            }
+
+            override fun onLost(network: Network?) {
+                networklivedata.postValue(false)
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        } else {
+            val builder = NetworkRequest.Builder()
+            connectivityManager.registerNetworkCallback(builder.build(), networkCallback)
+        }
+
+        return networklivedata
+    }
+}
---
+++ app/src/main/java/com/dev/adnetwrokmanager/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dev.adnetwrokmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adnetwrokmanager.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
